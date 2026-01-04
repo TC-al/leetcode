efd8005fd5c2f58bec40f91af2ae369596e14cc7
TC-al <tc.alanliu@gmail.com>
2026-01-03 20:08:58 -0500

    Time: 435 ms (12.55%) | Memory: 18.7 MB (77.00%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sumFourDivisors(self, nums: List[int]) -> int:
+        
+        result = 0
+        for num in nums:
+            divisors = 0
+            temp = 0
+            for i in range(1, math.floor(math.sqrt(num)) + 1):
+                if num % i == 0:
+                    divisors += 1
+                    temp += i
+                    if num / i != i:
+                        divisors += 1
+                        temp += num / i
+
+            print(divisors, temp)
+            if divisors == 4:
+                result += temp
+        
+        return int(result)
